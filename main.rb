0e560919d8a5c75068b4a3469873018e14b0ee5a
Gwen Larson <glarson284@gmail.com>
2015-06-27 23:02:28 -0400

    almost finisfinished testimonials page, including jquery

--- main.rb
+++ main.rb
@@ -1,17 +1,17 @@
 require 'sinatra'
-require 'mandrill'
-ENV['MANDRILL_APIKEY']
-m = Mandrill::API.new
-message = {
- :subject=> "Hello from the Mandrill API",
- :from_name=> "Also, James",
- :text=>"Hi message, how are you?",
- :to=>[{:email=> "middlemissj.usa@gmail.com", :name=> "James"}],
- :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
- :from_email=>"sender@yourdomain.com"
-}
-sending = m.messages.send message
-# puts sending
+# require 'mandrill'
+# ENV['MANDRILL_APIKEY']
+# m = Mandrill::API.new
+# message = {
+#  :subject=> "Hello from the Mandrill API",
+#  :from_name=> "Also, James",
+#  :text=>"Hi message, how are you?",
+#  :to=>[{:email=> "middlemissj.usa@gmail.com", :name=> "James"}],
+#  :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
+#  :from_email=>"sender@yourdomain.com"
+# }
+# sending = m.messages.send message
+# # puts sending
 
 require './user_array.rb'
 
@@ -29,7 +29,7 @@ end
 
 get '/profile' do
   @stylesheet = 'style/profile.css'
-  @zip = 90001
+  @zip = 10038
   erb :profile
 end
 
@@ -50,4 +50,11 @@ get '/browse' do
 
   erb :browse
 end
+
+get '/testimonial' do
+  @stylesheet = 'style/testimonials.css'
+  erb :testimonal  
+
+end
+
 
