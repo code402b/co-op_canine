f7db4e802f0c9791ef623bb4010f602d53f41af0
Anthony McGovern <mcgovern.anthony@gmail.com>
2015-06-28 23:10:40 -0400

    added dynamic profile test profile_popup.erb and browse_popup.erb

--- main.rb
+++ main.rb
@@ -1,19 +1,26 @@
 require 'sinatra'
-# require 'mandrill'
-# ENV['MANDRILL_APIKEY']
-# m = Mandrill::API.new
-# message = {
-#  :subject=> "Hello from the Mandrill API",
-#  :from_name=> "Also, James",
-#  :text=>"Hi message, how are you?",
-#  :to=>[{:email=> "middlemissj.usa@gmail.com", :name=> "James"}],
-#  :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
-#  :from_email=>"sender@yourdomain.com"
-# }
-# sending = m.messages.send message
-# # puts sending
+require 'mandrill'
+require 'json'
+ 
+ENV['MANDRILL_APIKEY']
+m = Mandrill::API.new
+message = {
+ :subject=> "Hello from the Mandrill API",
+ :from_name=> "Also, James",
+ :text=>"Hi message, how are you?",
+ :to=>[{:email=> "middlemissj.usa@gmail.com", :name=> "James"}],
+ :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",
+ :from_email=>"sender@yourdomain.com"
+}
+sending = m.messages.send message
 
-require './user_array.rb'
+# creates method to parse users.json into a ruby hash
+def users 
+  file = File.open("users.json", "r")
+  contents = file.read
+  users = JSON.parse(contents)
+end
+
 
 get '/' do
   @stylesheet = 'style/home.css'
@@ -22,39 +29,34 @@ end
 
 get '/about' do
   @stylesheet = 'style/home.css'
- erb :home
-
-end
-
-
-get '/profile' do
-  @stylesheet = 'style/profile.css'
-  @zip = 10011
-  erb :profile
-end
-
-get '/browse' do
-  @stylesheet = 'style/browse.css'
-
-  def users 
-  # return value should be the array of ruby hashes
-  # parse json file into ruby array
-
-  file = File.open("users.json", "r")
-  contents = file.read
-  users = JSON.parse(contents)
-
-  end
-
-  @users = users
-
-  erb :browse
+  erb :home
 end
 
 get '/testimonial' do
   @stylesheet = 'style/testimonials.css'
   erb :testimonal  
-
 end
+
+
+get '/profile' do
+  @stylesheet = 'style/profile.css'
+  @users = users
+  erb :profile
+end
+
+
+get '/browse' do
+  @stylesheet = 'style/browse.css'
+  @users = users
+  erb :browse
+end
+
+get '/browse_popup' do
+  @stylesheet = 'style/profile_popup.css'
+  @users = users
+  erb :browse_popup
+end
+
+
 
 
--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,43 +1,49 @@
 $(document).ready(function(){
-            $('#girl1').hide().fadeIn(3000); 
-            $('#girl2').hide().fadeIn(3000); 
-            $('#lady1').hide().fadeIn(3000);
-            $('#man2').hide().fadeIn(3000); 
-            $('.lines').hide().delay(1000).fadeIn(3000);   
-            $('#dog1').hide().delay(2000).fadeIn(3000);
-            $('#dog3').hide().delay(2000).fadeIn(3000);   
-            $('#guy1').hide().delay(3000).fadeIn(3000); 
-            $('#guy2').hide().delay(3000).fadeIn(3000); 
-            $('#lady2').hide().delay(3000).fadeIn(3000);
-            $('#man3').hide().delay(3000).fadeIn(3000); 
-            $('.lines2').hide().delay(4000).fadeIn(3000);   
-            $('#dog2').hide().delay(5000).fadeIn(3000);
-            $('#dog4').hide().delay(5000).fadeIn(3000);  
-            // $('#first').css({'background-color':'#428080','border-radius':'20px','padding':'10px'}),3000;  
-            // $('#second').css({'background-color':'#428080','border-radius':'20px','padding':'10px'}),3000;  
-            // $('#third').hide().css({'background-color':'#428080','border-radius':'20px','padding':'20px'}).show(3000),3000;  
-            // $('#fourth').hide().show(3000).css({'background-color':'#428080','border-radius':'20px','padding':'20px'}),3000;  
-            $(".testimonial-box").fadeTo(3000, 0.3, 'swing', function(){
-            $(this).css({"background-color":"#428080",'border-radius':'20px'});
-              }).delay(1000).fadeTo('slow', 1);
-           // $("#first").delay(4000).queue(function() {
-           //  $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
-           //   })
-           //  .fadeIn(3000);
-            // $("#second").delay(4000).queue(function() {
-            // $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
-            //  })
-            // .fadeIn(3000);
-            // $("#third").delay(4000).queue(function() {
-            // $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
-            //  })
-            // .fadeIn(3000);
-            // $("#fourth").delay(4000).queue(function() {
-            // $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
-            //  })
-            // .fadeIn(3000);
-           
-          });
+
+    $('#girl1').hide().fadeIn(3000); 
+    $('#girl2').hide().fadeIn(3000); 
+    $('#lady1').hide().fadeIn(3000);
+    $('#man2').hide().fadeIn(3000); 
+    $('.lines').hide().delay(1000).fadeIn(3000);   
+    $('#dog1').hide().delay(2000).fadeIn(3000);
+    $('#dog3').hide().delay(2000).fadeIn(3000);   
+    $('#guy1').hide().delay(3000).fadeIn(3000); 
+    $('#guy2').hide().delay(3000).fadeIn(3000); 
+    $('#lady2').hide().delay(3000).fadeIn(3000);
+    $('#man3').hide().delay(3000).fadeIn(3000); 
+    $('.lines2').hide().delay(4000).fadeIn(3000);   
+    $('#dog2').hide().delay(5000).fadeIn(3000);
+    $('#dog4').hide().delay(5000).fadeIn(3000);  
+    // $('#first').css({'background-color':'#428080','border-radius':'20px','padding':'10px'}),3000;  
+    // $('#second').css({'background-color':'#428080','border-radius':'20px','padding':'10px'}),3000;  
+    // $('#third').hide().css({'background-color':'#428080','border-radius':'20px','padding':'20px'}).show(3000),3000;  
+    // $('#fourth').hide().show(3000).css({'background-color':'#428080','border-radius':'20px','padding':'20px'}),3000;  
+    $(".testimonial-box").fadeTo(3000, 0.3, 'swing', function(){
+    $(this).css({"background-color":"#428080",'border-radius':'20px'});
+      }).delay(1000).fadeTo('slow', 1);
+   // $("#first").delay(4000).queue(function() {
+   //  $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
+   //   })
+   //  .fadeIn(3000);
+    // $("#second").delay(4000).queue(function() {
+    // $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
+    //  })
+    // .fadeIn(3000);
+    // $("#third").delay(4000).queue(function() {
+    // $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
+    //  })
+    // .fadeIn(3000);
+    // $("#fourth").delay(4000).queue(function() {
+    // $(this).css({"background-color":"#428080",'border-radius':'20px','padding':'10px'}).dequeue();
+    //  })
+    // .fadeIn(3000);
+    
+    $('#fadeandscale').popup({
+        transition: 'all 0.3s'
+    });       
+
+
+}); //closes doc.ready
 
 
          
